c6cbe529d2ee9447a8411820a52db63cd51233f6
Mauricio Rey <mauricioreygmail.com>
2024-04-05 11:31:19 -0500

    proyecto tablar-Productos

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('tablar::page')
+
+@section('content')
+    <!-- Page header -->
+        <h1> esta es la cabecera </h1>
+    <!-- Page body -->
+    <h1>  Calzados Bumerang cuerpo del home </h1>
+@endsection
